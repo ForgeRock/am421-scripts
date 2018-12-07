3b1062acd296be2e0664343a2cd3e958ba957d39
Peter Varga <peter.varga@dpc.hu>
2018-12-07 21:30:29 +0100

    fetch-tools.sh even more nicer output

--- fetch-tools.sh
+++ fetch-tools.sh
@@ -14,5 +14,5 @@ cd "$SCRIPT_DIR"
 logf "  Fetching latest version of scripts..."
 git clean -f -q
 git reset --hard --quiet master
-git pull -f -q 2>&1 | awk '{printf $0".."}'
+git pull -f -q 2>&1 | awk 'BEGIN {CHANGES=0} {if ($0 == "Already up-to-date.") {printf "No changes..."} else if (/\|/) { if (CHANGES == 0) {printf "Updating "$1} else { printf ", "$1}; CHANGES++}} END {if (CHANGES > 0) printf "..."}'
 logstatus
