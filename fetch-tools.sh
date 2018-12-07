4372285c41fa86cf3ccba0236159d5dbbf27c226
Peter Varga <peter.varga@dpc.hu>
2018-12-07 21:37:16 +0100

    removed initial spaces from fetch-tools.sh output

--- fetch-tools.sh
+++ fetch-tools.sh
@@ -11,7 +11,7 @@ source $SCRIPT_DIR/common.sh
 
 cd "$SCRIPT_DIR"
 
-logf "  Fetching latest version of scripts..."
+logf "Fetching latest version of scripts..."
 git clean -f -q
 git reset --hard --quiet master
 git pull -f -q 2>&1 | awk 'BEGIN {CHANGES=0} {if ($0 == "Already up-to-date.") {printf "No changes..."} else if (/\|/) { if (CHANGES == 0) {printf "Updating "$1} else { printf ", "$1}; CHANGES++}} END {if (CHANGES > 0) printf "..."}'
