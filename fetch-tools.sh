155d23d1fe11f5005f28b424302c2dd18d1291a1
Peter Varga <peter.varga@dpc.hu>
2018-12-07 17:00:40 +0100

    fetch-tools.sh added

---
+++ fetch-tools.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# AM421 script to check-out the state
+# checkout-state.sh
+# Parameters
+#  1. state to be checked out: lab01 ... lab12
+# 2016-2018. VRG
+# 2018.05. Pal
+# 2018.11 VRG
+SCRIPT_DIR="$( dirname "$( which "$0" )" )"
+source $SCRIPT_DIR/common.sh
+
+cd "$SCRIPT_DIR"
+
+logf "  Fetching latest version of tools..."
+git fetch --all > /dev/null
+logstatus
+logf "  Replacing modified files with fresh ones..."
+git checkout -f master > /dev/null
+logstatus
