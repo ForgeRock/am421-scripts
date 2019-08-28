61ef16b91170ebd51e2afe646ff9384dfea6500f
Peter Varga (VM) <peter.varga@dpc.hu>
2019-08-27 20:55:45 -0400

    Added seamonkey reset to this script...for AM 6.5.2 update

--- reset_browsers.sh
+++ reset_browsers.sh
@@ -12,12 +12,20 @@ logf "Shutting down Mozilla Firefox..."
 pkill firefox
 logstatus
 
+logf "Shutting down Mozilla Seamonkey..."
+pkill seamonkey
+logstatus
+
 logf "Deleting current Chrome settings and cache..."
 rm -rf ~/.config/google-chrome ~/.cache/google-chrome
 logstatus
 
-logf "Deleting current Firefox settings and chache..."
+logf "Deleting current Firefox settings and cache..."
 rm -rf ~/.mozilla/firefox ~/.cache/mozilla/firefox
+logstatus
+
+logf "Deleting current Seamonkey settings and cache..."
+rm -rf ~/.mozilla/seamonkey ~/.cache/mozilla/seamonkey
 logstatus
 
 logf "Replacing Chrome settings with the initial..."
@@ -30,5 +38,9 @@ cd /
 tar xjf "${COURSE_DIR}/backups/firefox-settings.tar.bz2"
 logstatus
 
+logf "Replacing Seamonkey settings with the initial..."
+cd /
+tar xjf "${COURSE_DIR}/backups/seamonkey-settings.tar.bz2"
+logstatus
 
 
